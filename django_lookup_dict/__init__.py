f6d27a82df0d76fab226cf528217f6ece0ceedee
hendawy <hendawy@outlook.com>
2017-06-16 08:50:30 +0200

    Changing license code comments

--- django_lookup_dict/__init__.py
+++ django_lookup_dict/__init__.py
@@ -5,17 +5,4 @@
 
 # This file is part of Django Lookup Dict.
 
-# Django Lookup Dict is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# Django Lookup Dict is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
-
 from lookup import LookupDict
